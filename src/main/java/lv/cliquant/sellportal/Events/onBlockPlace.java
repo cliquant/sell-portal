dc4af56609dc63f1175e2a012578e3f8c52b51b2
Niks <42009476+cliquant@users.noreply.github.com>
2022-12-31 04:36:08 +0200

    Update onBlockPlace.java

--- src/main/java/lv/cliquant/sellportal/Events/onBlockPlace.java
+++ src/main/java/lv/cliquant/sellportal/Events/onBlockPlace.java
@@ -2,11 +2,13 @@ package lv.cliquant.sellportal.Events;
 
 import com.jeff_media.customblockdata.CustomBlockData;
 import de.tr7zw.changeme.nbtapi.NBTItem;
+import eu.decentsoftware.holograms.api.DHAPI;
 import lv.cliquant.sellportal.Main;
 import lv.cliquant.sellportal.Manager.HookManager;
 import lv.cliquant.sellportal.Manager.ItemManager;
 import lv.cliquant.sellportal.Manager.SellPortalManager;
 import lv.cliquant.sellportal.Other.Utils;
+import me.filoghost.holographicdisplays.api.HolographicDisplaysAPI;
 import me.filoghost.holographicdisplays.api.hologram.Hologram;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.*;
@@ -20,7 +22,9 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 public class onBlockPlace implements Listener {
@@ -51,7 +55,7 @@ public class onBlockPlace implements Listener {
         Block placedBlock = event.getBlockPlaced();
         if (!player.hasPermission("sellportal.place"))
         if(player.getLocation().distance(placedBlock.getLocation()) < 3) {
-            player.sendMessage(Utils.Color(Main.getInstance().getConfig().getString("messages.take-distance").replace("{prefix}", Main.getInstance().getConfig().getString("messages.prefix")).replace("{player}", player.getDisplayName())));
+            player.sendMessage(Utils.Color(Main.getInstance().getConfig().getString("messages.take-distance").replace("{prefix}", Main.getInstance().getConfig().getString("messages.prefix")).replace("{player}", player.getName())));
             event.setCancelled(true);
             return;
         }
@@ -109,7 +113,7 @@ public class onBlockPlace implements Listener {
                 }
             }
         } else {
-            player.sendMessage(Utils.Color(Main.getInstance().getConfig().getString("messages.need-space").replace("{prefix}", Main.getInstance().getConfig().getString("messages.prefix")).replace("{player}", player.getDisplayName())));
+            player.sendMessage(Utils.Color(Main.getInstance().getConfig().getString("messages.need-space").replace("{prefix}", Main.getInstance().getConfig().getString("messages.prefix")).replace("{player}", player.getName())));
             event.setCancelled(true);
             return;
         }
@@ -120,13 +124,34 @@ public class onBlockPlace implements Listener {
         double y1 = placedBlock.getLocation().getY();
         double z1 = placedBlock.getLocation().getZ();
 
-        Hologram hologram = HookManager.api.createHologram(event.getBlockPlaced().getLocation().add( 0.5, 3.0, 0.5));
+        if(HookManager.HologramHook == 1) {
+            Location loc = event.getBlockPlaced().getLocation().add( 0.5, 3.0, 0.5);
 
-        for (String all : Main.config.getStringList("portal.hologram.lines")) {
-            hologram.getLines().appendText(ChatColor.translateAlternateColorCodes('&', all.replace("{owner}", player.getName()).replace("{totalsold}", SellPortalManager.soldSinceReboot.get(key) + "")));
+            Hologram hologram = HolographicDisplaysAPI.get(Main.getInstance()).createHologram(loc);
+
+            for (String all : Main.config.getStringList("portal.hologram.lines")) {
+                hologram.getLines().appendText(org.bukkit.ChatColor.translateAlternateColorCodes('&', all.replace("{owner}", player.getName())).replace("{totalsold}", SellPortalManager.soldSinceReboot.get(key) + ""));
+            }
+
+            SellPortalManager.sellPortals1.put(key, hologram);
         }
+        if(HookManager.HologramHook == 2) {
+            if(DHAPI.getHologram(key) != null) {
+                DHAPI.getHologram(key).delete();
+            }
 
-        SellPortalManager.sellPortals.put(key, hologram);
+            Location loc = event.getBlockPlaced().getLocation().add( 0.5, 3.0, 0.5);
+
+            List<String> lines = new ArrayList<>();
+
+            for (String all : Main.config.getStringList("portal.hologram.lines")) {
+                lines.add(org.bukkit.ChatColor.translateAlternateColorCodes('&', all.replace("{owner}", player.getName())).replace("{totalsold}", SellPortalManager.soldSinceReboot.get(key) + ""));
+            }
+
+            eu.decentsoftware.holograms.api.holograms.Hologram hologram = DHAPI.createHologram(key, loc, lines);
+
+            SellPortalManager.sellPortals2.put(key, hologram);
+        }
 
         Main.data.set("portals." + key + ".owner", player.getName());
         Main.data.set("portals." + key + ".world", w.getName());
