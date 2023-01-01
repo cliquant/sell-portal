df826f9bf7177fa2460686bdaf4272014a235324
Niks <42009476+cliquant@users.noreply.github.com>
2023-01-01 17:44:26 +0200

    Update onPlayerTeleport.java

--- src/main/java/lv/cliquant/sellportal/Events/onPlayerTeleport.java
+++ src/main/java/lv/cliquant/sellportal/Events/onPlayerTeleport.java
@@ -3,6 +3,7 @@ package lv.cliquant.sellportal.Events;
 import com.jeff_media.customblockdata.CustomBlockData;
 import lv.cliquant.sellportal.Main;
 import lv.cliquant.sellportal.Manager.SellPortalManager;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
@@ -33,8 +34,14 @@ public class onPlayerTeleport implements Listener {
     public void playerTeleport(PlayerTeleportEvent event) {
         if (event.getCause().equals(PlayerTeleportEvent.TeleportCause.END_PORTAL)) {
             Block block = event.getPlayer().getLocation().getBlock();
+            Block block1 = event.getPlayer().getLocation().add(0, 1, 0).getBlock();
+            Block block2 = event.getPlayer().getLocation().add(0, 2, 0).getBlock();
+            Block block3 = event.getPlayer().getLocation().add(0, 3, 0).getBlock();
             PersistentDataContainer container = new CustomBlockData(block, Main.getInstance());
-            if(container.has(sellPortalKey, PersistentDataType.STRING)) {
+            PersistentDataContainer container1 = new CustomBlockData(block1, Main.getInstance());
+            PersistentDataContainer container2 = new CustomBlockData(block2, Main.getInstance());
+            PersistentDataContainer container3 = new CustomBlockData(block3, Main.getInstance());
+            if(container.has(sellPortalKey, PersistentDataType.STRING) || container1.has(sellPortalKey, PersistentDataType.STRING) || container2.has(sellPortalKey, PersistentDataType.STRING) || container3.has(sellPortalKey, PersistentDataType.STRING)) {
                 event.setCancelled(true);
             }
         }
